83d84aaf578b17486b6ba3ffa060198bd25667dc
Scot-Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2026-04-30 21:23:20 +0200

    chore: re-enable docking

--- main.cpp
+++ main.cpp
@@ -306,7 +306,7 @@ static bool init_app(AppState& state)
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-    // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 #ifndef __EMSCRIPTEN__
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
     io.ConfigViewportsNoAutoMerge = true;
